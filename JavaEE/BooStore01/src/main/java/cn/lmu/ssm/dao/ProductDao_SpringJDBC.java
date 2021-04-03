480e529ed3549a51c664781ffc161126f3fb2670
ChenZhe <ChenZhe@DESKTOP-8EOSRRI>
2021-04-03 18:51:23 +0800

    JavaEE第四周作业

--- JavaEE/BooStore01/src/main/java/cn/lmu/ssm/dao/ProductDao_SpringJDBC.java
+++ JavaEE/BooStore01/src/main/java/cn/lmu/ssm/dao/ProductDao_SpringJDBC.java
@@ -1,7 +1,13 @@
 package cn.lmu.ssm.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -23,14 +29,34 @@ public class ProductDao_SpringJDBC extends ProductDao {
 
 //	@Autowired
 //	private PlatformTransactionManager transactionManager;
+
+	private SqlSessionFactory sqlSessionFactory = null;
+
+	public ProductDao_SpringJDBC() throws IOException {
+		// 不使用Spring方式创建，反之可以直接用注解由Spring自动创建
+		// 1. 创建SqlSessionFactoryBuilder对象
+		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+		// 2. 加载SqlMapConfig.xml配置文件
+		InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+		// 3. 创建SqlSessionFactory对象
+		this.sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+	}
+
 	@Override
 	public List<Product> getProducts() {
-		// 定义SQL语句
-		String sql = "select * from product";
-		// 创建一个新的BeanPropertyRowMapper对象
-		RowMapper<Product> rowMapper = new BeanPropertyRowMapper<Product>(Product.class);
-		// 执行静态的SQL查询，并通过RowMapper返回结果
-		return this.jdbcTemplate.query(sql, rowMapper);
+//		// 定义SQL语句
+//		String sql = "select * from product";
+//		// 创建一个新的BeanPropertyRowMapper对象
+//		RowMapper<Product> rowMapper = new BeanPropertyRowMapper<Product>(Product.class);
+//		// 执行静态的SQL查询，并通过RowMapper返回结果
+//		return this.jdbcTemplate.query(sql, rowMapper);
+
+		// 4. 创建SqlSession对象
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		// 5. 执行SqlSession对象执行查询，获取结果User
+		// 第一个参数是product.xml的statement的id；
+		List<Product> products = sqlSession.selectList("getProducts");
+		return products;
 	}
 
 	@Override
@@ -90,10 +116,14 @@ public class ProductDao_SpringJDBC extends ProductDao {
 	}
 
 	public Product find(int id) {
-		String sql = "SELECT * FROM product WHERE id=?";
-//		Product product = this.jdbcTemplate.queryForList(sql, new Object[] { id });
-		RowMapper<Product> rowMapper = new BeanPropertyRowMapper<Product>(Product.class);
-		return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
-//		return product;
+//		String sql = "SELECT * FROM product WHERE id=?";
+////		Product product = this.jdbcTemplate.queryForList(sql, new Object[] { id });
+//		RowMapper<Product> rowMapper = new BeanPropertyRowMapper<Product>(Product.class);
+//		return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
+////		return product;
+
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		Product product = sqlSession.selectOne("getProductsById", id);
+		return product;
 	}
 }
